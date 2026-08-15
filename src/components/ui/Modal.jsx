b8544948d77392c15ccf1cb098f635bd6859d203
mfk2252 <mfk2252@gmail.com>
2026-08-15 15:34:01 +0300

    أي نقر على الخلفية الداكنة لن يُغلق النافذة أبداً

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,6 +1,6 @@
 export default function Modal({ id, size='', children, onClose }) {
   return (
-    <div className="mbg" onClick={e => { if(e.target === e.currentTarget && onClose) onClose(); }}>
+    <div className="mbg">
       <div className={`mb ${size}`}>
         {children}
       </div>
