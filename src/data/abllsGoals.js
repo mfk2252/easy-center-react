cd539b5c5bccdf1cb4cde08d2d9458b7c6a946f1
mfk2252 <mfk2252@gmail.com>
2026-08-18 12:20:53 +0300

    اضافة اهداف الايبلز

---
+++ src/data/abllsGoals.js
@@ -0,0 +1,92 @@
+/**
+ * Master Index for ABLLS-R Protocol Goals
+ * Contains all 25 official sections (A to Z excluding O)
+ */
+import { ABLLS_SECTION_A, ABLLS_SECTION_B, ABLLS_SECTION_C, ABLLS_SECTION_D, ABLLS_SECTION_E } from './abllsGoalsFoundational';
+import { ABLLS_SECTION_F, ABLLS_SECTION_G, ABLLS_SECTION_H, ABLLS_SECTION_I, ABLLS_SECTION_J } from './abllsGoalsLanguage';
+import { ABLLS_SECTION_K, ABLLS_SECTION_L, ABLLS_SECTION_M, ABLLS_SECTION_N, ABLLS_SECTION_P, ABLLS_SECTION_Q, ABLLS_SECTION_R, ABLLS_SECTION_S, ABLLS_SECTION_T } from './abllsGoalsAcademic';
+import { ABLLS_SECTION_U, ABLLS_SECTION_V, ABLLS_SECTION_W, ABLLS_SECTION_X, ABLLS_SECTION_Y, ABLLS_SECTION_Z } from './abllsGoalsDailyLivingMotor';
+
+export {
+  ABLLS_SECTION_A,
+  ABLLS_SECTION_B,
+  ABLLS_SECTION_C,
+  ABLLS_SECTION_D,
+  ABLLS_SECTION_E,
+  ABLLS_SECTION_F,
+  ABLLS_SECTION_G,
+  ABLLS_SECTION_H,
+  ABLLS_SECTION_I,
+  ABLLS_SECTION_J,
+  ABLLS_SECTION_K,
+  ABLLS_SECTION_L,
+  ABLLS_SECTION_M,
+  ABLLS_SECTION_N,
+  ABLLS_SECTION_P,
+  ABLLS_SECTION_Q,
+  ABLLS_SECTION_R,
+  ABLLS_SECTION_S,
+  ABLLS_SECTION_T,
+  ABLLS_SECTION_U,
+  ABLLS_SECTION_V,
+  ABLLS_SECTION_W,
+  ABLLS_SECTION_X,
+  ABLLS_SECTION_Y,
+  ABLLS_SECTION_Z
+};
+
+export const ALL_ABLLS_GOALS = [
+  ...ABLLS_SECTION_A,
+  ...ABLLS_SECTION_B,
+  ...ABLLS_SECTION_C,
+  ...ABLLS_SECTION_D,
+  ...ABLLS_SECTION_E,
+  ...ABLLS_SECTION_F,
+  ...ABLLS_SECTION_G,
+  ...ABLLS_SECTION_H,
+  ...ABLLS_SECTION_I,
+  ...ABLLS_SECTION_J,
+  ...ABLLS_SECTION_K,
+  ...ABLLS_SECTION_L,
+  ...ABLLS_SECTION_M,
+  ...ABLLS_SECTION_N,
+  ...ABLLS_SECTION_P,
+  ...ABLLS_SECTION_Q,
+  ...ABLLS_SECTION_R,
+  ...ABLLS_SECTION_S,
+  ...ABLLS_SECTION_T,
+  ...ABLLS_SECTION_U,
+  ...ABLLS_SECTION_V,
+  ...ABLLS_SECTION_W,
+  ...ABLLS_SECTION_X,
+  ...ABLLS_SECTION_Y,
+  ...ABLLS_SECTION_Z
+];
+
+export const ABLLS_DOMAINS_LIST = [
+  { key: 'ablls_a', label: '[A] التعاون وفاعلية المعزز' },
+  { key: 'ablls_b', label: '[B] الأداء البصري' },
+  { key: 'ablls_c', label: '[C] اللغة الاستقبالية' },
+  { key: 'ablls_d', label: '[D] التقليد الحركي' },
+  { key: 'ablls_e', label: '[E] التقليد اللفظي' },
+  { key: 'ablls_f', label: '[F] الطلب (Mands)' },
+  { key: 'ablls_g', label: '[G] التسمية (Tacts)' },
+  { key: 'ablls_h', label: '[H] استخدام الألفاظ (Intraverbals)' },
+  { key: 'ablls_i', label: '[I] النطق التلقائي' },
+  { key: 'ablls_j', label: '[J] التراكيب اللغوية والنحوية' },
+  { key: 'ablls_k', label: '[K] مهارات اللعب ووقت الفراغ' },
+  { key: 'ablls_l', label: '[L] التفاعل والتواصل الاجتماعي' },
+  { key: 'ablls_m', label: '[M] إتباع التعليمات والتفاعل ضمن مجموعة' },
+  { key: 'ablls_n', label: '[N] الروتين والنظام داخل الفصل' },
+  { key: 'ablls_p', label: '[P] تعميم الاستجابات' },
+  { key: 'ablls_q', label: '[Q] مهارات القراءة' },
+  { key: 'ablls_r', label: '[R] مهارات الحساب والرياضيات' },
+  { key: 'ablls_s', label: '[S] مهارات الكتابة والنسخ' },
+  { key: 'ablls_t', label: '[T] مهارات الهجاء' },
+  { key: 'ablls_u', label: '[U] مهارات ارتداء وخلع الملابس' },
+  { key: 'ablls_v', label: '[V] مهارات تناول الطعام' },
+  { key: 'ablls_w', label: '[W] الهندام ورعاية الذات' },
+  { key: 'ablls_x', label: '[X] دخول الحمام والتواليت' },
+  { key: 'ablls_y', label: '[Y] مهارات الحركية الكبرى' },
+  { key: 'ablls_z', label: '[Z] مهارات الحركية الصغرى' },
+];
